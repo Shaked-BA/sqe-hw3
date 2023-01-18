e464a46c257fdae84da84ce5fb101075e8e15f74
Gad Miller <gadmiller7@gmail.com>
2023-01-18 18:41:08 +0200

    Commenting Works

---
+++ Provengo/Prestashop/spec/js/PrestashopCommenting.EventDef.js
@@ -0,0 +1,54 @@
+/* @Provengo summon selenium */
+
+/**
+ * Useful commands:
+ * bp.log.info(e);
+ * provengo report <path-to-project>
+ * Use of asserts in provengo
+ */
+
+/**
+ *  The goToProduct event defines the selenium actions for entering a product page.
+ */
+defineEvent(SeleniumSession, "goToProduct", function(session, e) {
+    session.click("//*[@id='content']/section[1]/div/div[1]/article/div/div[1]/a/img");
+})
+
+/**
+ * The leaveReview event defines the selenium actions for giving a review for a product.
+ */
+defineEvent(SeleniumSession, "leaveReview", function(session, e) {
+    session.click("//*[@id='product-comments-list-footer']/button");
+    session.writeText("//*[@id='post-product-comment-form']/div[2]/div[1]/input", e.title);
+    session.writeText("//*[@id='post-product-comment-form']/div[2]/div[2]/input", e.name);
+    session.writeText("//*[@id='post-product-comment-form']/div[4]/div/textarea". e.content);
+    session.click("//*[@id='post-product-comment-form']/div[6]/div[2]/button[2]");
+})
+
+/**
+ * The adminLogin event defines the selenium actions for admin login to the system.
+ */
+defineEvent(SeleniumSession, "adminLogin", function(session, e) {
+    session.writeText("//*[@id='email']", e.username);
+    session.writeText("//*[@id='passwd']", e.password);
+    session.click("//*[@id='submit_login']");
+})
+
+/**
+ * The navigateToModule event defines the selenium actions for reaching a specific module settings.
+ */
+defineEvent(SeleniumSession, "navigateToModule", function(session, e) {
+    session.click("//*[@id='header_infos']/i");
+    session.click("//*[@id='subtab-AdminParentModulesSf']/a/span");
+    session.click("//*[@id='subtab-AdminModulesSf']/a");
+    session.writeText("//*[@id='search-input-group']/div[1]/div[2]/input", e.module);
+    session.click("//*[@id='module-search-button']");
+})
+
+/**
+ * The turnOffCommenting event defines the selenium actions for turning commenting off.
+ */
+defineEvent(SeleniumSession, "turnOffCommenting", function(session, e) {
+    session.click("//*[@id='modules-list-container-theme_modules']/div/div/div/div[2]/div[4]/div[2]/a");
+    session.click("//*[@id='PRODUCT_COMMENTS_ALLOW_GUESTS_off']");
+})
